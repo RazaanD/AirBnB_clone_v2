7af0373478b2351256d579799920cd8fe3745f6a
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-12-23 00:37:58 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
-# Setup webservers for web static AirBnB_clone_V2
-apt-get update
-apt-get -y install nginx
-ufw allow 'Nginx HTTP'
-mkdir -p /data/web_static/{releases/test,shared}
-cat >> /data/web_static/releases/test/index.html << EOF
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-EOF
-# Create symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Adjust permissions
-chown ubuntu:ubuntu -R /data
-sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-# Restart nginx
-service nginx restart
-# Exit with 0 code
-exit 0
+# Set up server file system for deployment
+
+# install nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
+
+# configure file system
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# set permissions
+sudo chown -R ubuntu:ubuntu /data/
+
+# configure nginx
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+# restart web server
+sudo service nginx restart
